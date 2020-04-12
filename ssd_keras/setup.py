9c2aed95c4decaf4a44b987bcc2b7b1aa90a88d5
yelkhattabi <yelkhattabi@scortex.io>
2020-04-12 14:36:37 +0200

    Yassine : module correction

--- ssd_keras/setup.py
+++ ssd_keras/setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup 
+from setuptools import setup, find_packages
 
 setup(
     name = "ssd_keras",
     version = "0.0.1",
     description = "ssd_keras_library",
     py_module = ["ssd_keras"],
-    package_dir = {'':'ssd_keras'}
+    packages = find_packages(),
 )
