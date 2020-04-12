c95f4fabfa669a4d80e4a2b269b46559b1341c92
yelkhattabi <yelkhattabi@scortex.io>
2020-04-12 13:35:26 +0200

    Yassine: commit ssd_keras library cleaned

---
+++ ssd_keras/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup 
+
+setup(
+    name = "ssd_keras",
+    version = "0.0.1",
+    description = "ssd_keras_library",
+    py_module = ["ssd_keras"],
+    package_dir = {'':'ssd_keras'}
+)
